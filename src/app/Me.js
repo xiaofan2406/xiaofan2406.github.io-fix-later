4ca831deba92305d076ff4c71012deb9143a86c7
Xiaofan Wu <Xiaofan2406@gmail.com>
2016-10-20 16:30:52 +1100

    fix typo

--- src/app/Me.js
+++ src/app/Me.js
@@ -11,9 +11,9 @@ function Me() {
       title: 'Xiaofan Wu',
       content: (
         <div>
-          <p>I am a junior developer based in Melbourne.</p><p> I aim to becoming a
+          <p>I am a junior developer based in Melbourne.</p><p> I aim to become a
             full-stack <code>JavaScript</code> developer.</p>
-          <p>Game development and Software Engineering are my secondary interests.</p>
+          <p>Game development and Software Engineering are my secondary interest.</p>
         </div>
       )
     },
