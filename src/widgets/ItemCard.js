6e95919f5127b1a694b35d5b27c13d53ec1ad235
Xiaofan Wu <Xiaofan2406@gmail.com>
2016-11-03 09:17:52 +1100

    ehhh some fonts

--- src/widgets/ItemCard.js
+++ src/widgets/ItemCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card } from 'antd';
+import Anchor from 'widgets/blank-anchor';
 
 import './ItemCard.css';
 
@@ -10,15 +11,19 @@ function ItemCard({ item, height, ...rest }) {
       {...rest}
       className="ItemCard"
       title={item.name}
-      extra={item.type === 'static' ? '' : (
-        <a href={item.link} target="_blank" rel="noopener noreferrer" title={item.name}>View</a>
-      )}
       style={{ height }}
     >
       <div
         // eslint-disable-next-line react/no-danger
         dangerouslySetInnerHTML={{ __html: item.content }}
       />
+      {item.type !== 'static' && (
+        <div className="ItemCard-action">
+          <Anchor href={item.link} title={item.name}>
+            <i className="fa fa-arrow-right" aria-hidden="true" />
+          </Anchor>
+        </div>
+      )}
     </Card>
   );
 }
